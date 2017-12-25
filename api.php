f3a1e9490e5cac4cde9c4bf6ccd53e2786984eb2
IvanLuLyf <835498692@qq.com>
2017-12-26 01:14:45 +0800

    add RESTful API

--- api.php
+++ api.php
@@ -5,8 +5,9 @@
  * Date: 2017/10/23
  * Time: 13:46
  */
-$url = $_SERVER['REQUEST_URI'];
-echo ($url."<br>");
-echo ($_GET['mod']."<br>");
-echo ($_GET['action']."<br>");
-echo ($_SESSION['token']."<br>");
+define('APP_PATH', __DIR__ . '/');
+define('APP_DEBUG', true);
+define("IN_TWIMI_PHP", "True", TRUE);
+require(APP_PATH . 'TwimiPHP/TwimiPHP.php');
+$config = require(APP_PATH . 'config/config.php');
+(new TwimiPHP($config,1))->run();
