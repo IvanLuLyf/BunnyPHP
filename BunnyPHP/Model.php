bc9a9e5730511bff4b1f8a7b28ebb410f4178e0e
IvanLuLyf <835498692@qq.com>
2018-10-05 23:10:31 +0800

    Update Model Class,Add SQLite Support

--- BunnyPHP/Model.php
+++ BunnyPHP/Model.php
@@ -6,203 +6,117 @@
  * Time: 1:03
  */
 
-class Model implements ArrayAccess
+class Model
 {
-    protected $table;
-    protected $model;
-    private $filter = '';
-    private $join = '';
-    private $param = array();
-    protected $data = array();
+    protected $_table;
+    protected $_name;
+    private $_filter = '';
+    private $_join = '';
+    private $_param = [];
+    private $_has_where = false;
 
-    public function __construct($params = array())
+    public function __construct()
     {
-        if (!$this->table) {
-            $this->model = get_class($this);
-            $this->model = substr($this->model, 0, -5);
-            $this->table = 'tp_' . strtolower($this->model);
-        }
-        foreach ($params as $k => $v) {
-            $this->$k = $v;
-            $this->data[$k] = $v;
+        if (!$this->_table) {
+            $this->_name = substr(get_class($this), 0, -5);
+            $this->_table = DB_PREFIX . strtolower($this->_name);
         }
     }
 
-    public function where($where, $param = array())
+    private function reset()
+    {
+        $this->_filter = '';
+        $this->_join = '';
+        $this->_param = [];
+        $this->_has_where = false;
+    }
+
+    public function where($where, $param = [])
     {
         if ($where) {
-            $this->filter .= ' Where ';
+            $this->_has_where = true;
             if (is_array($where)) {
-                $this->filter .= implode(' ', $where);
+                $this->_filter .= implode(' ', $where);
             } else {
-                $this->filter .= $where;
+                $this->_filter .= $where;
             }
-            $this->param = $param;
+            $this->_param = $param;
         }
         return $this;
     }
 
-    public function join($tableName, $condition = array(), $mod = '')
+    public function join($tableName, $condition = [], $mod = '')
     {
-        $this->join .= " $mod Join `$tableName`";
-        if ($condition) {
-            $this->join .= " On (" . implode(' ', $condition) . ") ";
+        $this->_join .= " $mod join `$tableName`";
+        if (is_array($condition)) {
+            $this->_join .= " on (" . implode(' ', $condition) . ") ";
+        } else {
+            $this->_join .= " on ($condition) ";
         }
         return $this;
     }
 
     public function limit($size, $start = 0)
     {
-        $this->filter .= " Limit $start,$size ";
+        $this->_filter .= " limit $start,$size ";
         return $this;
     }
 
-    public function order($order = array())
+    public function order($order = [])
     {
-        if ($order) {
-            $this->filter .= ' Order By ';
-            $this->filter .= implode(',', $order);
+        if (is_array($order)) {
+            $this->_filter .= ' order by ';
+            $this->_filter .= implode(',', $order);
+        } else {
+            $this->_filter .= " order by $order ";
         }
         return $this;
-    }
-
-    public function getAll($column = '*')
-    {
-        $rows = $this->fetchAll($column);
-        $result = array();
-        $modelName = get_class($this);
-        foreach ($rows as $row) {
-            $result[] = new $modelName($row);
-        }
-        return $result;
-    }
-
-    public function get($column = '*')
-    {
-        $row = $this->fetch($column);
-        $modelName = get_class($this);
-        $newThis = new $modelName($row);
-        $newThis->data = $row;
-        return $newThis;
-    }
-
-    public function fetchAll($column = '*')
-    {
-        $sql = sprintf("Select %s From `%s` %s %s", $column, $this->table, $this->join, $this->filter);
-        $sth = Database::pdo()->prepare($sql);
-        $sth = $this->formatParam($sth, $this->param);
-        $sth->execute();
-        $this->join = '';
-        $this->filter = '';
-        $this->param = array();
-        return $sth->fetchAll();
     }
 
     public function fetch($column = '*')
     {
-        $sql = sprintf("Select %s From `%s` %s %s", $column, $this->table, $this->join, $this->filter);
-        $sth = Database::pdo()->prepare($sql);
-        $sth = $this->formatParam($sth, $this->param);
-        $sth->execute();
-        $this->join = '';
-        $this->filter = '';
-        $this->param = array();
-        return $sth->fetch();
+        if (is_array($column)) {
+            $column = implode(',', $column);
+        }
+        if ($this->_has_where) {
+            $this->_filter = ' where ' . $this->_filter;
+        }
+        $sql = "select {$column} from `{$this->_table}` {$this->_join} {$this->_filter}";
+        $result = Database::getInstance()->fetchOne($sql, $this->_param);
+        $this->reset();
+        return $result;
+    }
+
+    public function fetchAll($column = '*')
+    {
+        if (is_array($column)) {
+            $column = implode(',', $column);
+        }
+        if ($this->_has_where) {
+            $this->_filter = ' where ' . $this->_filter;
+        }
+        $sql = "select {$column} from `{$this->_table}` {$this->_join} {$this->_filter}";
+        $result = Database::getInstance()->fetchAll($sql, $this->_param);
+        $this->reset();
+        return $result;
     }
 
     public function delete()
     {
-        $sql = sprintf("Delete From `%s` Where %s", $this->table, $this->filter);
-        $sth = Database::pdo()->prepare($sql);
-        $sth = $this->formatParam($sth, $this->param);
-        $sth->execute();
-        $this->join = '';
-        $this->filter = '';
-        $this->param = array();
-        return $sth->rowCount();
+        $result = Database::getInstance()->delete($this->_table, $this->_filter, $this->_param);
+        $this->reset();
+        return $result;
     }
 
-    public function add($data)
+    public function add($data = [])
     {
-        $sql = sprintf("Insert Into `%s` %s", $this->table, $this->formatInsert($data));
-        $sth = Database::pdo()->prepare($sql);
-        $sth = $this->formatParam($sth, $data);
-        $sth = $this->formatParam($sth, $this->param);
-        $sth->execute();
-        $this->join = '';
-        $this->filter = '';
-        $this->param = array();
-        return Database::pdo()->lastInsertId();
+        Database::getInstance()->insert($data, $this->_table);
     }
 
-    public function update($data)
+    public function update($data = [])
     {
-        $sql = sprintf("Update `%s` Set %s %s", $this->table, $this->formatUpdate($data), $this->filter);
-        $sth = Database::pdo()->prepare($sql);
-        $sth = $this->formatParam($sth, $data);
-        $sth = $this->formatParam($sth, $this->param);
-        $sth->execute();
-        $this->join = '';
-        $this->filter = '';
-        $this->param = array();
-        return $sth->rowCount();
-    }
-
-    private function formatParam(PDOStatement $sth, $params = array())
-    {
-        foreach ($params as $param => &$value) {
-            $param = is_int($param) ? $param + 1 : ':' . ltrim($param, ':');
-            $sth->bindParam($param, $value);
-        }
-        return $sth;
-    }
-
-    private function formatInsert($data)
-    {
-        $fields = array();
-        $names = array();
-        foreach ($data as $key => $value) {
-            $fields[] = sprintf("`%s`", $key);
-            $names[] = sprintf(":%s", $key);
-        }
-        $field = implode(',', $fields);
-        $name = implode(',', $names);
-        return sprintf("(%s) values (%s)", $field, $name);
-    }
-
-    private function formatUpdate($data)
-    {
-        $fields = array();
-        foreach ($data as $key => $value) {
-            $fields[] = sprintf("`%s` = :%s", $key, $key);
-        }
-        return implode(',', $fields);
-    }
-
-    public function __get($name)
-    {
-        return $this->data[$name];
-    }
-
-    public function offsetExists($offset)
-    {
-        return isset($this->$offset) || isset($this->data[$offset]);
-    }
-
-    public function offsetGet($offset)
-    {
-        return $this->$offset || $this->data[$offset];
-    }
-
-    public function offsetSet($offset, $value)
-    {
-        $this->$offset = $value;
-        $this->data[$offset] = $value;
-    }
-
-    public function offsetUnset($offset)
-    {
-        unset($this->$offset);
-        unset($this->data[$offset]);
+        $result = Database::getInstance()->update($data, $this->_table, $this->_filter, $this->_param);
+        $this->reset();
+        return $result;
     }
 }
