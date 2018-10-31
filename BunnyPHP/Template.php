d2c143c43bff821d99da4aa7ba1611f3ee186e53
IvanLuLyf <835498692@qq.com>
2018-10-31 15:22:36 +0800

    Add Template

---
+++ BunnyPHP/Template.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IvanLu
+ * Date: 2018/10/31
+ * Time: 14:50
+ */
+
+class Template
+{
+    protected $template;
+    protected $content;
+
+    public function __construct($template)
+    {
+        if (file_exists(APP_PATH . "template/{$template}.tpl.html")) {
+            $this->template = $template;
+            $this->content = file_get_contents(APP_PATH . "template/{$template}.tpl.html");
+        }
+    }
+
+    public function compile($output = '')
+    {
+        if ($output == '') {
+            $output = APP_PATH . "template/{$this->template}.html";
+        }
+        $this->parse_var();
+        file_put_contents($output, $this->content);
+    }
+
+    private function parse_var()
+    {
+        $pattern = '/\{\{\s+([\w]+)\s+\}\}/';
+        if (preg_match($pattern, $this->content)) {
+            $this->content = preg_replace($pattern, "<?=\$$1?>", $this->content);
+        }
+        $pattern = '/\{\{\s+([\w]+).([\w]+)\s+\}\}/';
+        if (preg_match($pattern, $this->content)) {
+            $this->content = preg_replace($pattern, "<?=\$$1['$2']?>", $this->content);
+        }
+    }
+}
