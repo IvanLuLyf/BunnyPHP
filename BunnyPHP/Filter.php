404dcc1980f1877249a00cdd00a7ef44d1b97a31
IvanLuLyf <835498692@qq.com>
2018-11-12 01:12:45 +0800

    Update Filter

--- BunnyPHP/Filter.php
+++ BunnyPHP/Filter.php
@@ -11,8 +11,30 @@ class Filter
     const NEXT = 0;
     const STOP = 1;
 
+    private $_mode;
+
+    public function __construct($mode = BunnyPHP::MODE_NORMAL)
+    {
+        $this->_mode = $mode;
+    }
+
     public function doFilter()
     {
         return self::NEXT;
+    }
+
+    public function render($variable, $template = '')
+    {
+        View::render($template, $variable, $this->_mode);
+    }
+
+    public function error($variable)
+    {
+        View::error($variable, $this->_mode);
+    }
+
+    public function redirect($url, $action = null, $params = [])
+    {
+        View::redirect($url, $action, $params);
     }
 }
