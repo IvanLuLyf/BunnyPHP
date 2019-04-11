b855d6b6517f1b88e9a0bffe54571b3dc5c3fdf8
IvanLuLyf <835498692@qq.com>
2019-04-12 00:32:12 +0800

    Update Config

--- BunnyPHP/Config.php
+++ BunnyPHP/Config.php
@@ -11,6 +11,7 @@ class Config
     const MODE_CONST = 0;
     const MODE_ARRAY = 1;
     const MODE_JSON = 2;
+    const MODE_SERIAL = 3;
 
     private $configs = [];
 
@@ -42,23 +43,33 @@ class Config
         }
     }
 
-    public static function check($name)
+    public static function check($name, $mode = self::MODE_ARRAY, $basePath = APP_PATH . "config/")
     {
-        return file_exists(APP_PATH . "config/{$name}.php");
+        if ($mode == self::MODE_CONST || $mode == self::MODE_ARRAY)
+            return file_exists("{$basePath}{$name}.php");
+        elseif ($mode == self::MODE_JSON)
+            return file_exists("{$basePath}{$name}.json");
+        elseif ($mode == self::MODE_SERIAL)
+            return file_exists("{$basePath}{$name}.serial");
+        else
+            return false;
     }
 
-    public static function checkLock($name)
+    public static function checkLock($name, $basePath = APP_PATH . "config/")
     {
-        return file_exists(APP_PATH . "config/{$name}.lock");
+        return file_exists("{$basePath}{$name}.lock");
     }
 
-    public static function load($name)
+    public static function load($name, $basePath = APP_PATH . "config/")
     {
-        if (file_exists(APP_PATH . "config/{$name}.php")) {
-            return new self(require APP_PATH . "config/{$name}.php");
-        } elseif (file_exists(APP_PATH . "config/{$name}.json")) {
-            $configJSON = file_get_contents(APP_PATH . "config/{$name}.json");
+        if (file_exists("{$basePath}{$name}.php")) {
+            return new self(require "{$basePath}{$name}.php");
+        } elseif (file_exists("{$basePath}{$name}.json")) {
+            $configJSON = file_get_contents("{$basePath}{$name}.json");
             return new self(json_decode($configJSON, true));
+        } elseif (file_exists("{$basePath}{$name}.serial")) {
+            $configSerial = file_get_contents("{$basePath}{$name}.serial");
+            return new self(unserialize($configSerial));
         } else {
             return new self();
         }
@@ -68,24 +79,28 @@ class Config
     {
         $config_text = "<?php\r\n";
         if ($type == self::MODE_CONST) {
-            foreach ($configs as $k => $v) {
-                $config_text .= "define(\"{$k}\",\"{$v}\");\r\n";
-            }
+            $config_text .= self::make_const_config($configs, '');
+        } elseif ($type == self::MODE_JSON) {
+            $config_text = json_encode($configs);
+        } elseif ($type == self::MODE_SERIAL) {
+            $config_text = serialize($configs);
         } else {
-            $config_text .= "return [\r\n";
-            foreach ($configs as $k => $v) {
-                $config_text .= "\"{$k}\"=>";
-                if (is_array($v)) {
-                    $config_text .= "\t[\r\n";
-                    foreach ($v as $k2 => $v2) {
-                        $config_text .= "\t\"{$k2}\"=>\"{$v2}\",\r\n";
-                    }
-                    $config_text .= "],\r\n";
-                } else {
-                    $config_text .= "\"{$v}\",\r\n";
-                }
+            $config_text .= "return " . var_export($configs, true) . ";\r\n";
+        }
+        return $config_text;
+    }
+
+    private static function make_const_config($configs = [], $nameSpace = '')
+    {
+        $config_text = '';
+        foreach ($configs as $k => $v) {
+            if (is_array($v)) {
+                $config_text .= self::make_const_config($v, $k . '_');
+            } elseif (is_int($v)) {
+                $config_text .= "define('" . strtoupper($nameSpace . $k) . "',$v);\r\n";
+            } else {
+                $config_text .= "define('" . strtoupper($nameSpace . $k) . "','" . addslashes($v) . "');\r\n";
             }
-            $config_text .= '];';
         }
         return $config_text;
     }
