c3a35eac29f5827fa6991f19b2c48ea756701602
IvanLuLyf <835498692@qq.com>
2019-04-12 00:32:56 +0800

    update config sample

--- config/config_sample.php
+++ config/config_sample.php
@@ -1,19 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: IvanLu
- * Date: 2017/10/5
- * Time: 16:02
- */
-$config['db']['host'] = 'localhost';
-$config['db']['username'] = 'username';
-$config['db']['password'] = 'password';
-$config['db']['database'] = 'database';
-$config['site_name'] = 'YourSiteName';
-$config['site_url'] = 'YourDomain.com';
-$config['controller'] = 'Index';
-$config['action'] = 'index';
-$config['storage'] = [
-    'name' => 'file'
+return [
+    'db' => [
+        'type' => 'mysql',
+        'host' => 'localhost',
+        'port' => '3306',
+        'username' => 'username',
+        'password' => 'password',
+        'database' => 'database',
+        'prefix' => 'tp_',
+    ],
+    'site_name' => 'Your Site Name',
+    'site_url' => 'YourDomain.com',
+    'controller' => 'Index',
+    'storage' => [
+        'name' => 'file'
+    ]
 ];
-return $config;
