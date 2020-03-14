3b72de14e9a47656fabbe6ca5893bbcd1a1e1e5a
IvanLuLyf <835498692@qq.com>
2020-03-14 16:05:16 +0800

    update 2.3.4

--- BunnyPHP/BunnyPHP.php
+++ BunnyPHP/BunnyPHP.php
@@ -14,7 +14,7 @@ use ReflectionException;
 
 class BunnyPHP
 {
-    const BUNNY_VERSION = '2.3.3';
+    const BUNNY_VERSION = '2.3.4';
     const MODE_NORMAL = 0;
     const MODE_API = 1;
     const MODE_AJAX = 2;
