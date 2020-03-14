a390b3d3eafa10c6414aec15003aabcdc19059ec
IvanLuLyf <835498692@qq.com>
2020-03-14 16:23:55 +0800

    update 2.3.5 Template update for language

--- BunnyPHP/BunnyPHP.php
+++ BunnyPHP/BunnyPHP.php
@@ -14,7 +14,7 @@ use ReflectionException;
 
 class BunnyPHP
 {
-    const BUNNY_VERSION = '2.3.4';
+    const BUNNY_VERSION = '2.3.5';
     const MODE_NORMAL = 0;
     const MODE_API = 1;
     const MODE_AJAX = 2;
