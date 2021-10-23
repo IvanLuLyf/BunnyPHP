14a8530a998061e1c4a97763f71f10af51e7cc92
IvanLuLyf <835498692@qq.com>
2021-10-23 15:36:46 +0800

    update Request

--- BunnyPHP/Request.php
+++ BunnyPHP/Request.php
@@ -24,31 +24,54 @@ class Request implements ArrayAccess
         return null;
     }
 
-    public function getSession($name)
+    public static function cookie($nameOrValue, $value = '', $expire = 0, $path = '/', $domain = '', $httpOnly = true)
+    {
+        $time = time() - 1;
+        if (is_array($nameOrValue)) {
+            foreach ($nameOrValue as $k => $v) {
+                setcookie($k, $v ?? '', $v === null ? $time : $expire, $path, $domain, false, $httpOnly);
+            }
+        } else if ($value !== '') {
+            setcookie($nameOrValue, $value ?? '', $value === null ? $time : $expire, $path, $domain, false, $httpOnly);
+        } else if (isset($_COOKIE[$nameOrValue])) {
+            return $_COOKIE[$nameOrValue];
+        }
+        return null;
+    }
+
+    public static function session($nameOrValue, $value = '')
     {
         if (!session_id()) session_start();
-        if (empty($_SESSION[$name]) === false) {
-            return $_SESSION[$name];
-        } else {
-            return null;
+        if (is_array($nameOrValue)) {
+            foreach ($nameOrValue as $k => $v) {
+                if ($v === null) unset($_SESSION[$k]);
+                else$_SESSION[$k] = $v;
+            }
+        } else if ($value !== '') {
+            if ($value === null) {
+                $tmp = $_SESSION[$nameOrValue] ?? null;
+                unset($_SESSION[$nameOrValue]);
+                return $tmp;
+            } else $_SESSION[$nameOrValue] = $value;
+        } else if (isset($_SESSION[$nameOrValue])) {
+            return $_SESSION[$nameOrValue];
         }
+        return null;
+    }
+
+    public function getSession($name)
+    {
+        return self::session($name);
     }
 
     public function setSession($name, $value)
     {
-        if (!session_id()) session_start();
-        $_SESSION[$name] = $value;
+        self::session($name, $value);
     }
 
     public function delSession($name)
     {
-        if (!session_id()) session_start();
-        $ret = null;
-        if (empty($_SESSION[$name]) === false) {
-            $ret = $_SESSION[$name];
-        }
-        unset($_SESSION[$name]);
-        return $ret;
+        return self::session($name, null);
     }
 
     public function process()
@@ -56,11 +79,11 @@ class Request implements ArrayAccess
         if ($this->processed || !isset($_SERVER['CONTENT_TYPE'])) return;
         $content = file_get_contents('php://input');
         $contentType = strtolower($_SERVER['CONTENT_TYPE']);
-        if (strpos($contentType, 'application/json') === 0) {
+        if (strpos($contentType, 'application/json') >= 0) {
             $this->param = json_decode($content, true);
-        } elseif (strpos($contentType, 'application/x-www-form-urlencoded') === 0) {
+        } elseif (strpos($contentType, 'application/x-www-form-urlencoded') >= 0) {
             parse_str($content, $this->param);
-        } elseif (strpos($contentType, 'multipart/form-data;') === 0) {
+        } elseif (strpos($contentType, 'multipart/form-data;') >= 0) {
             if (strtolower($_SERVER['REQUEST_METHOD']) === 'post') {
                 $this->param = $_POST;
                 return;
