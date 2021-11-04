079758fe84c4b4493d57d392b0f85d80dbe6f6c0
IvanLuLyf <835498692@qq.com>
2021-11-04 23:38:02 +0800

    release 3.1.0

--- BunnyPHP/BunnyPHP.php
+++ BunnyPHP/BunnyPHP.php
@@ -10,7 +10,7 @@ use ReflectionMethod;
 
 class BunnyPHP
 {
-    const BUNNY_VERSION = '3.0.9';
+    const BUNNY_VERSION = '3.1.0';
     const MODE_NORMAL = 0;
     const MODE_API = 1;
     const MODE_AJAX = 2;
